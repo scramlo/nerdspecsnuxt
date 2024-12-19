e6825563f900cfcce10a2342bd292dce2dbaf703
Brian Scramlin <brian@nerdspecs.com>
2024-12-19 14:10:18 -0500

    feat: add blog

--- app.vue
+++ app.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-
 const colorMode = useColorMode()
 
 const darkMode = ref(true);
@@ -28,13 +27,50 @@ const links = [{
   label: 'Portfolio',
   icon: 'i-heroicons-squares-2x2',
   to: '#portfolio'
-}]
+}];
+
+const query = gql`
+query {
+  publication(host: "blog.nerdspecs.com") {
+    title
+    posts(first: 3) {
+      edges {
+        node {
+          title
+          subtitle
+          coverImage {
+            url
+          }
+          url,
+          readTimeInMinutes
+        }
+      }
+    }
+  }
+}`;
+
+type BlogPost = {
+  title: string;
+  subtitle: string;
+  coverImage: {
+    url: string;
+  };
+  url: string;
+  readTimeInMinutes: number;
+};
+
+const { data } = await useAsyncQuery<{ publication: { posts: { edges: { node: BlogPost }[] } } }>(query);
+
+const blogPosts = computed(() => {
+  if (!data?.value) return [];
+  return data.value.publication.posts.edges.map((edge) => edge.node);
+});
 
 </script>
 <template>
-<div class="flex">
+<div class="flex flex-col lg:flex-row">
   <!-- Sidebar -->
-  <aside class="flex flex-col h-screen w-1/6 p-5 gap-4 bg-secondary">
+  <aside class="flex flex-col w-full lg:w-1/6 p-5 gap-4 bg-secondary">
     <section class="h-full flex flex-col justify-between">
       <!-- Top portion of sidebar -->
       <div>
@@ -59,7 +95,7 @@ const links = [{
 
     </div>
   </aside>
-  <main class="flex flex-col w-full pt-5 mt-20 px-10">
+  <main class="flex flex-col w-full px-10">
     <!-- About Me -->
     <section class="flex gap-4 justify-center items-center">
       <div class="w-1/2 flex justify-center">
@@ -84,10 +120,59 @@ const links = [{
     </section>
 
     <!-- Blog -->
-    <section></section>
+    <section class="bg-gray-100 dark:bg-gray-800">
+      <header>
+        <h2 class="text-3xl font-bold mb-10">Latest Blog Posts</h2>
+      </header>
+      <div class="flex gap-4">
+        <!-- featured post -->
+         <article class="flex flex-col gap-2">
+            <a :href="blogPosts[0]?.url">
+              <img 
+                :src="blogPosts[0]?.coverImage.url"
+                :alt="blogPosts[0]?.title"
+              />
+            </a>
+            <h3 class="text-3xl">{{ blogPosts[0]?.title }}</h3>
+            <p class="text-xl">{{ blogPosts[0]?.subtitle }}</p>
+            <p class="flex items-center gap-1"><UIcon name="i-heroicons-book-open" /> {{ blogPosts[0]?.readTimeInMinutes }} min read</p>
+         </article>
+         <!-- next two articles -->
+         <div class="flex flex-col gap-4">
+          <article class="flex flex-col gap-2">
+              <a :href="blogPosts[1]?.url">
+              <img 
+                :src="blogPosts[1]?.coverImage.url"
+                :alt="blogPosts[1]?.title"
+              />
+              </a>
+            <h3 class="text-3xl">{{ blogPosts[1]?.title }}</h3>
+            <p class="text-xl">{{ blogPosts[1]?.subtitle }}</p>
+            <p class="flex items-center gap-1"><UIcon name="i-heroicons-book-open" /> {{ blogPosts[0]?.readTimeInMinutes }} min read</p>
+         </article>
+         <article class="flex flex-col gap-2">
+            <a :href="blogPosts[2]?.url">
+              <img 
+                :src="blogPosts[2]?.coverImage.url"
+                :alt="blogPosts[2]?.title"
+              />
+            </a>
+            <h3 class="text-3xl">{{ blogPosts[2]?.title }}</h3>
+            <p class="text-xl">{{ blogPosts[2]?.subtitle }}</p>
+            <p class="flex items-center gap-1"><UIcon name="i-heroicons-book-open" /> {{ blogPosts[0]?.readTimeInMinutes }} min read</p>
+         </article>
+         </div>
+      </div>
+    </section>
 
     <!-- Portfolio -->
     <section></section>
   </main>
 </div>
 </template>
+
+<style scoped>
+section {
+  @apply p-10;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"],
-  compatibilityDate: "2024-12-18",
+  modules: ['@nuxt/ui', '@nuxtjs/apollo'],
+  compatibilityDate: '2024-12-18',
+  apollo: {
+    clients: {
+      default: {
+        httpEndpoint: 'https://gql.hashnode.com/',
+        httpLinkOptions: {
+          headers: {
+            'Authorization': '7a1122e0-214b-4dd5-905e-68b5eab1e487'
+          }
+        }
+      }
+    },
+  },
 })
