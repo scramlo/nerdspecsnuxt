65c301c89b963716a48ed57bcdb0079feb7b7b8c
Brian Scramlin <brian.scramlin@gmail.com>
2025-01-09 10:20:52 -0500

    add favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,16 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxtjs/apollo', '@nuxt/content'],
   compatibilityDate: '2024-12-18',
+  app: {
+    head: {
+      link: [
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+      ]
+    }
+  },
   apollo: {
     clients: {
       default: {
